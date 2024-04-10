5a553d5f1418ee2bf3c2d9831fb33d00034396b2
unknown <johnndegimwangi1@gmail.com>
2024-04-10 17:18:20 +0300

    added header

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import CarList from "./assets/Components/CarsList";
+import Header from "./assets/Components/Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <CarList />
 
       <StatusBar style="auto" />
